b42270364faf4137332456521766f21eee13e022
shanegibney <shanegibney@gmail.com>
2018-08-18 13:14:48 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>Parent component</h1>
+    <Child/>
+  </div>
+</template>
+
+<script>
+import Child from './components/Child'
+export default {
+  name: 'App',
+  components: {
+    Child
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,53 @@
+<template lang="html">
+  <div>
+    <h1>{{ title }}</h1>
+    <button @click="alterTitle()">Change title</button>
+    <h2> <a href="">Return to repo</a> </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+  data () {
+    return {
+      title: 'Old title'
+    }
+  },
+  methods: {
+    alterTitle () {
+      this.title = 'Much improved title'
+    }
+  },
+  beforeCreate () {
+    alert('beforeCreate: before componet is created')
+  },
+  created () {
+    alert('created: component created but not yet added to DOM')
+  },
+  beforeMount () {
+    alert('beforeMount: before component is mounted to the DOM but when it has been created')
+  },
+  mounted () {
+    alert('mounted: after component is mounted to the DOM. This doesn\'t work for me')
+  },
+  beforeUpdate () {
+    alert('beforeUpdate: before data is changed if it is about to change')
+  },
+  updated () {
+    alert('updated: after data has been changed')
+  },
+  beforeDestroy () {
+    alert('beforeDestroy: before component is destroyed')
+  },
+  destroy () {
+    alert('destroy: after component has been destroyed')
+  }
+}
+</script>
+
+<style lang="css" scoped>
+h1 {
+  color: lightblue;
+}
+</style>
